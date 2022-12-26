8e521ccbb98fdd97bc9bd7ef53df30e05d7fca5b
Marsha Kordhawerti <marshak116@gmail.com>
2022-12-26 11:14:01 +0700

    Update load.php

--- app/Views/student/load.php
+++ app/Views/student/load.php
@@ -7,43 +7,42 @@
       <th>Rayon</th>
       <th>Rombel</th>
       <th>Aksi</th>
-      <th>Dipinjam</th>
     </tr>
   </thead>
   <tbody>
     <?php
-      $no = 1;
-      foreach ($students as $student) {
-        ?>
-          <tr>
-            <td scope="row"><?= $no++ ?></td>
-            <td><?= $student->nis ?></td>
-            <td><?= $student->nama_siswa ?></td>
-            <?php
-              foreach ($rayons as $rayon) {
-                if ($rayon->rayon_id == $student->rayon_id) {
-                  ?>
-                    <td><?= $rayon->nama_rayon ?></td>
-                  <?php
-                }
-              }
-            ?>
-            <?php
-              foreach ($rombels as $rombel) {
-                if ($rombel->rombel_id == $student->rombel_id) {
-                  ?>
-                    <td><?= $rombel->nama_rombel ?></td>
-                  <?php
-                }
-              }
-            ?>
-            <td class="row justify-content-around">
-              <button class="btn btn-warning" onclick="editStudent('<?= $student->student_id ?>')">Edit</button>
-              <button class="btn btn-danger" onclick="return (confirm('Yakin Hapus Data Siswa <?= $student->nama_siswa ?> ?')) ? deleteStudent('<?= $student->student_id ?>') : '' ">Hapus</button>
-            </td>
-          </tr>
+    $no = 1;
+    foreach ($students as $student) {
+    ?>
+      <tr>
+        <td scope="row"><?= $no++ ?></td>
+        <td><?= $student->nis ?></td>
+        <td><?= $student->nama_siswa ?></td>
         <?php
-      }
+        foreach ($rayons as $rayon) {
+          if ($rayon->rayon_id == $student->rayon_id) {
+        ?>
+            <td><?= $rayon->nama_rayon ?></td>
+        <?php
+          }
+        }
+        ?>
+        <?php
+        foreach ($rombels as $rombel) {
+          if ($rombel->rombel_id == $student->rombel_id) {
+        ?>
+            <td><?= $rombel->nama_rombel ?></td>
+        <?php
+          }
+        }
+        ?>
+        <td class="row justify-content-around">
+          <button class="btn btn-warning" onclick="editStudent('<?= $student->student_id ?>')">Edit</button>
+          <button class="btn btn-danger" onclick="return (confirm('Yakin Hapus Data Siswa <?= $student->nama_siswa ?> ?')) ? deleteStudent('<?= $student->student_id ?>') : '' ">Hapus</button>
+        </td>
+      </tr>
+    <?php
+    }
     ?>
   </tbody>
 </table>
